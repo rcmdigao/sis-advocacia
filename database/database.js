fc20ac2c59b3a9eaf8c904b17eed89632f837471
Rodrigo Cunha Machado <rodrigo.cmachado@gmail.com>
2020-12-21 22:33:28 -0300

    Preparando o ambiente para subir ao provedor

--- database/database.js
+++ database/database.js
@@ -1,6 +1,15 @@
 const Sequelize = require("sequelize");
+
+/* Conexão Dev
 const connection = new Sequelize("ldadvocacia", "root", "@m010283", {
   host: "localhost",
+  dialect: "mysql",
+});
+*/
+
+/* Conexão Prod */
+const connection = new Sequelize("sisadvocacia", "sisadvocacia", "62jzKD2JqqJDULx", {
+  host: "mysql.sisadvocacia.kinghost.net",
   dialect: "mysql",
 });
 
